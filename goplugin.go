5715ec58223d30f54c2149a8ad4625986749dc9f
leomick <83542885+leomick@users.noreply.github.com>
2024-11-17 16:52:41 -0500

    Don't extract if file/directory is to be discarded

--- goplugin.go
+++ goplugin.go
@@ -120,28 +120,31 @@ func ExtractTarGZ(compressedStream io.Reader, directory string, renamer func(str
 	tarReader := tar.NewReader(gzipReader)
 	var header *tar.Header
 	for header, err = tarReader.Next(); err == nil; header, err = tarReader.Next() {
-		path := filepath.Join(directory, renamer(header.Name))
-		switch header.Typeflag {
-		case tar.TypeDir:
-			if err := os.Mkdir(path, 0755); err != nil {
-				return fmt.Errorf("ExtractTarGz: Mkdir() failed: %w", err)
-			}
-		case tar.TypeReg:
-			outFile, err := os.Create(path)
-			if err != nil {
-				return fmt.Errorf("ExtractTarGz: Create() failed: %w", err)
-			}
+		renamerResult := renamer(header.Name)
+		if renamerResult != "" {
+			path := filepath.Join(directory, renamerResult)
+			switch header.Typeflag {
+			case tar.TypeDir:
+				if err := os.Mkdir(path, 0755); err != nil {
+					return fmt.Errorf("ExtractTarGz: Mkdir() failed: %w", err)
+				}
+			case tar.TypeReg:
+				outFile, err := os.Create(path)
+				if err != nil {
+					return fmt.Errorf("ExtractTarGz: Create() failed: %w", err)
+				}
 
-			if _, err := io.Copy(outFile, tarReader); err != nil {
-				// outFile.Close error omitted as Copy error is more interesting at this point
-				outFile.Close()
-				return fmt.Errorf("ExtractTarGz: Copy() failed: %w", err)
+				if _, err := io.Copy(outFile, tarReader); err != nil {
+					// outFile.Close error omitted as Copy error is more interesting at this point
+					outFile.Close()
+					return fmt.Errorf("ExtractTarGz: Copy() failed: %w", err)
+				}
+				if err := outFile.Close(); err != nil {
+					return fmt.Errorf("ExtractTarGz: Close() failed: %w", err)
+				}
+			default:
+				return fmt.Errorf("ExtractTarGz: uknown type: %b in %s", header.Typeflag, header.Name)
 			}
-			if err := outFile.Close(); err != nil {
-				return fmt.Errorf("ExtractTarGz: Close() failed: %w", err)
-			}
-		default:
-			return fmt.Errorf("ExtractTarGz: uknown type: %b in %s", header.Typeflag, header.Name)
 		}
 	}
 	if err != io.EOF {
